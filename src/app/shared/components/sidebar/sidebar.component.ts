882e68ed3cdc4817b05b74e191ce10bef857ae3e
Wiris Wernek <wiriswernek@gmail.com>
2024-07-12 12:03:26 -0300

    feat: add sidebar component

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NgbActiveOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { MenuModel } from '../../../models/menu.model';
+
+@Component({
+	selector: 'app-sidebar',
+	standalone: true,
+	imports: [],
+	templateUrl: './sidebar.component.html',
+	styleUrl: './sidebar.component.scss',
+})
+export class SidebarComponent {
+	constructor(public activeOffcanvas: NgbActiveOffcanvas) {}
+	items: MenuModel[] = [
+		{ nome: 'Atividades', icon: 'bi-activity', url: 'cliente/atividades' },
+		{ nome: 'Estatísticas', icon: 'bi-graph-up-arrow', url: 'cliente/estatisticas' },
+		{ nome: 'Importar Dados', icon: 'bi-database-up', url: 'cliente/importar' },
+		{ nome: 'Usuário', icon: 'bi-person', url: 'cliente/usuario' },
+	];
+}
