52ac07ba441f1a0b1dbee1e67c922209292cc098
Wiris Wernek <wiriswernek@gmail.com>
2024-07-12 12:04:56 -0300

    feat: add tela de importar dados

---
+++ src/app/views/importar-dados/importar-dados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-importar-dados',
+  standalone: true,
+  imports: [],
+  templateUrl: './importar-dados.component.html',
+  styleUrl: './importar-dados.component.scss'
+})
+export class ImportarDadosComponent {
+
+}
