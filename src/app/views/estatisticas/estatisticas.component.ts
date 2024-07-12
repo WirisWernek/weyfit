a2416b94dd25cadd738879919c138d3e1c07ad1a
Wiris Wernek <wiriswernek@gmail.com>
2024-07-12 12:05:42 -0300

    feat: add tela de estatísticas

---
+++ src/app/views/estatisticas/estatisticas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estatisticas',
+  standalone: true,
+  imports: [],
+  templateUrl: './estatisticas.component.html',
+  styleUrl: './estatisticas.component.scss'
+})
+export class EstatisticasComponent {
+
+}
