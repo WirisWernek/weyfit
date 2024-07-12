4d5649d58a9dae521f55830e5564cb83102854be
Wiris Wernek <wiriswernek@gmail.com>
2024-07-12 12:11:09 -0300

    feat: adiciona angular fire, ajusta skipTest na criação de components,  configuração do eslint e adiciona providers globais

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideClientHydration } from '@angular/platform-browser';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { environment } from '../environments/environment';
+
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideAnimations(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+	provideHttpClient(),
+  ],
 };
