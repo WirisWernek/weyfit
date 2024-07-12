9e3352d91b4a408a52068028906a8e42ae6ba768
Wiris Wernek <wiriswernek@gmail.com>
2024-07-11 23:19:11 -0300

    feat: add estilos globais e configurações de ambiente

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+	production: true,
+	firebase: {
+		projectId: 'weyfit-dde6c',
+		appId: '1:819961115242:web:b04db2b0c30daafa605013',
+		storageBucket: 'weyfit-dde6c.appspot.com',
+		apiKey: 'AIzaSyCK9Ca7yXq-_IfNxl8lKW73J4te63ahdQA',
+		authDomain: 'weyfit-dde6c.firebaseapp.com',
+		messagingSenderId: '819961115242',
+		measurementId: 'G-75R23EQ777',
+	},
+	defaultModalOptions: {
+		centered: true,
+	},
+};
